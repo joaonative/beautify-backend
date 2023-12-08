49fbbdbbdd2d71ba2084dd8f25696ac51c694610
joaonative <jestudos127@gmail.com>
2023-12-07 22:45:49 -0300

    feat: new list by brand route

--- routes/produto.js
+++ routes/produto.js
@@ -5,12 +5,16 @@ import {
   deletarProduto,
   listarProdutoPorId,
   listarProdutos,
+  listarProdutoPorMarca,
 } from "../controllers/produtoController.js";
 
 const router = express.Router();
 
 router.get("/", listarProdutos);
+router.get("/:marca", listarProdutoPorMarca);
 router.post("/", criarProduto);
 router.put("/", editarProduto);
 router.get("/:id", listarProdutoPorId);
 router.delete("/:id", deletarProduto);
+
+export default router;
